ba3ba9279b14832b7008368faef4613868f933c0
imxieyi <wez733@live.cn>
2017-03-21 02:17:50 +0800

    修改乱码

--- harib/bootpack.c
+++ harib/bootpack.c
@@ -7,11 +7,11 @@ void HariMain(void)
 
 	init_gdtidt();
 	init_pic();
-	io_sti();//堯?CPU?惗拞抐丆場?IDT/PIC涍?弶巒壔
+	io_sti();	//取消CPU中断禁止，因为IDT/PIC已初始化完成
 	init_palette();
 	init_screen(binfo->vram,binfo->scrnx,binfo->scrny);
 	
-	putfonts8_asc(binfo->vram,binfo->scrnx,8,8,COL8_FFFFFF,"ABC 123 敻敻");
+	putfonts8_asc(binfo->vram,binfo->scrnx,8,8,COL8_FFFFFF,"ABC 123");
 	putfonts8_asc(binfo->vram,binfo->scrnx,31,31,COL8_000000,"Project OS.");
 	putfonts8_asc(binfo->vram,binfo->scrnx,30,30,COL8_FFFFFF,"Project OS.");
 	
@@ -24,8 +24,8 @@ void HariMain(void)
 	init_mouse_cursor8(mcursor,COL8_008484);
 	putblock8_8(binfo->vram,binfo->scrnx,16,16,mx,my,mcursor,16);
 
-	io_out8(PIC0_IMR, 0xf9);//堯?PIC1榓???惗拞抐
-	io_out8(PIC1_IMR, 0xef);//堯?憀??惗拞抐
+	io_out8(PIC0_IMR, 0xf9);	//允许PIC1和键盘发送中断(11111001)
+	io_out8(PIC1_IMR, 0xef);	//允许鼠标发送中断(11101111)
 	
 	for (;;) {
 		io_hlt();
