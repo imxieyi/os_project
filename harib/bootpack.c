93855ab872e792c02576c7bf63c6a0c8921f9a76
imxieyi <wez733@live.cn>
2017-04-25 22:21:28 +0800

    图层处理
    第10天结束，然而出了点谜之bug，暂时忽略

--- harib/bootpack.c
+++ harib/bootpack.c
@@ -11,8 +11,10 @@ void HariMain(void)
 	init_gdtidt();
 	init_pic();
 	io_sti();//取消CPU中断禁止，因为IDT/PIC已初始化完成
-	init_palette();
-	init_screen(binfo->vram,binfo->scrnx,binfo->scrny);
+
+	struct SHEETCTRL *shtctl;
+	struct SHEET *sht_back,*sht_mouse;
+	unsigned char *buf_back,buf_mouse[256];
 	
 	unsigned int memtotal;
 	struct MEMMAN *memman=(struct MEMMAN *)MEMMAN_ADDR;
@@ -21,13 +23,28 @@ void HariMain(void)
 	memman_free(memman,0x00001000,0x0009e000);
 	memman_free(memman,0x00400000,memtotal-0x00400000);
 	char s[15];
-	sprintf(s,"mem: %dMB free:%dKB",memtotal/0x400000*4,memman_total(memman)/1024);
-	putfonts8_asc(binfo->vram,binfo->scrnx,16,64,COL8_840084,s);
+
+	init_palette();
+	shtctl=sheetctrl_init(memman,binfo->vram,binfo->scrnx,binfo->scrny);
+	sht_back=sheet_alloc(shtctl);
+	sht_mouse=sheet_alloc(shtctl);
+	buf_back=(unsigned char *)memman_alloc_4k(memman,binfo->scrnx*binfo->scrny);
+	sheet_setbuf(sht_back,buf_back,binfo->scrnx,binfo->scrny,-1);
+	sheet_setbuf(sht_mouse,buf_mouse,16,16,99);
+	init_screen(buf_back,binfo->scrnx,binfo->scrny);
 	
-	char *mcursor;
-	int mx=152,my=78;
-	init_mouse_cursor8(mcursor,COL8_008484);
-	putblock8_8(binfo->vram,binfo->scrnx,16,16,mx,my,mcursor,16);
+	int mx=(binfo->scrnx-16)/2,my=(binfo->scrny-28-16)/2;
+	init_mouse_cursor8(buf_mouse,99);
+	
+	sheet_slide(shtctl,sht_back,0,0);
+	sheet_slide(shtctl,sht_mouse,mx,my);
+	sheet_updown(shtctl,sht_back,0);
+	sheet_updown(shtctl,sht_mouse,1);
+	sprintf(s,"(%3d,%3d)",mx,my);
+	putfonts8_asc(buf_back,binfo->scrnx,0,0,COL8_FFFFFF,s);
+	sprintf(s,"mem: %dMB free:%dKB",memtotal/0x400000*4,memman_total(memman)/1024);
+	putfonts8_asc(buf_back,binfo->scrnx,0,32,COL8_840084,s);
+	sheet_refresh(shtctl,sht_back,0,0,binfo->scrnx,48);
 
 	init_keyboard();
 	struct MOUSE_DEC mdec;
@@ -50,8 +67,9 @@ void HariMain(void)
 				i=fifo8_get(&keybuf);
 				io_sti();
 				sprintf(s,"%02X",i);
-				boxfill8(binfo->vram,binfo->scrnx,COL8_008484,0,16,15,31);
-				putfonts8_asc(binfo->vram,binfo->scrnx,0,16,COL8_FFFFFF,s);
+				boxfill8(buf_back,binfo->scrnx,COL8_008484,0,16,15,31);
+				putfonts8_asc(buf_back,binfo->scrnx,0,16,COL8_FFFFFF,s);
+				sheet_refresh(shtctl,sht_back,0,16,16,32);
 			} else if(fifo8_status(&mousebuf)!=0){
 				i=fifo8_get(&mousebuf);
 				io_sti();
@@ -63,10 +81,10 @@ void HariMain(void)
 						s[3]='R';
 					if(mdec.btn&0x04)
 						s[2]='C';
-					boxfill8(binfo->vram,binfo->scrnx,COL8_008484,32,16,32+15*8-1,31);
-					putfonts8_asc(binfo->vram,binfo->scrnx,32,16,COL8_FFFFFF,s);
+					boxfill8(buf_back,binfo->scrnx,COL8_008484,32,16,32+15*8-1,31);
+					putfonts8_asc(buf_back,binfo->scrnx,32,16,COL8_FFFFFF,s);
+					sheet_refresh(shtctl,sht_back,32,16,32+15*8,32);
 					//移动鼠标指针
-					boxfill8(binfo->vram,binfo->scrnx,COL8_008484,mx,my,mx+15,my+15);
 					mx+=mdec.x;
 					my+=mdec.y;
 					mx=mx<0?0:mx;
@@ -74,9 +92,10 @@ void HariMain(void)
 					mx=mx>binfo->scrnx-16?binfo->scrnx-16:mx;
 					my=my>binfo->scrny-16?binfo->scrny-16:my;
 					sprintf(s,"(%3d,%3d)",mx,my);
-					boxfill8(binfo->vram,binfo->scrnx,COL8_008484,0,0,79,15);
-					putfonts8_asc(binfo->vram,binfo->scrnx,0,0,COL8_FFFFFF,s);
-					putblock8_8(binfo->vram,binfo->scrnx,16,16,mx,my,mcursor,16);
+					boxfill8(buf_back,binfo->scrnx,COL8_008484,0,0,79,15);
+					putfonts8_asc(buf_back,binfo->scrnx,0,0,COL8_FFFFFF,s);
+					sheet_refresh(shtctl,sht_back,0,0,80,16);
+					sheet_slide(shtctl,sht_mouse,mx,my);
 				}
 			}
 		}
